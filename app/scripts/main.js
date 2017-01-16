ee06c6be51f0b22f9aa0ea12d169c45e42e75c78
Rodrigo Puente <rodrigoramos89@gmail.com>
2017-01-16 11:37:56 -0600

    About to launch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('\'Allo \'Allo!');
+const background1 = '#4ABDAC';
+const background2 = '#FC4A1A';
+const background3 = '#F7B733';
+
+const body = document.querySelector('body');
+const portfolioContent = document.querySelector('#portfolio-content');
+const wcidContent = document.querySelector('#what-can-i-do-content');
+const otherWorkContent = document.querySelector('#other-work-content');
+
+inView.threshold(0.1);
+
+inView('#portfolio')
+  .on('enter', () => {
+  	body.style.background = background1;
+  });
+
+inView('#what-can-i-do')
+  .on('enter', () => {
+  	body.style.background = background2;
+  });
+
+inView('#other-work')
+  .on('enter', () => {
+  	body.style.background = background3;
+  });
+
+$(function() {
+	
+	var $grid = $('#portfolio-grid').imagesLoaded( function() {
+	  $grid.masonry({
+	  	itemSelector: '.portfolio-item'
+	  });
+	});
+
+  $('.img-container').perfectScrollbar();
+
+});
